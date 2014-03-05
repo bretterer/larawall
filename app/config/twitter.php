7c38d8f235aa274a1b2b6b1b40fdaf0085b7ef2a
Brian Retterer <bretterer@gmail.com>
2014-03-04 22:59:25 -0500

    update twitter.php config

--- app/config/twitter.php
+++ app/config/twitter.php
@@ -3,9 +3,9 @@
 return [
     'query2' => '(from:@daytonwp OR @daytonwp OR #wcdayton OR (#wcdayton AND #bingo) ) AND exclude:retweets',
     'query' => '#LittleCouple exclude:retweets',
-    'consumer_key'      => getenv('twitter.apikey'),
-    'consumer_secret'   => getenv('twitter.apisecret'),
-    'token'             => getenv('twitter.accesstoken'),
-    'token_secret'      => getenv('twitter.accesstokensecret'),
+    'consumer_key'      => getenv('twitter_apikey'),
+    'consumer_secret'   => getenv('twitter_apisecret'),
+    'token'             => getenv('twitter_accesstoken'),
+    'token_secret'      => getenv('twitter_accesstokensecret'),
     'ignore_rt'         => true
 ];
