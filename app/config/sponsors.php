19e6f70c398c59b177de8935458e8686526aabe1
Brian Retterer <bretterer@gmail.com>
2014-03-06 21:59:56 -0500

    Update schedule for Friday

--- app/config/sponsors.php
+++ app/config/sponsors.php
@@ -12,5 +12,57 @@ return [
     [
         "sponsor" => "ServerPress.com",
         "image" => "http://2014.dayton.wordcamp.org/files/2014/02/serverpress-logo-300x80.png"
+    ],
+    [
+        "sponsor" => "Constant Contact",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/03/ctct_logo_stacked_CMYK-300x298.jpg"
+    ],
+    [
+        "sponsor" => "The Ohlmann Group",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/The-Ohlmann-Group-300x77.jpg"
+    ],
+    [
+        "sponsor" => "New Media Dayton",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/NMDlogo-251x100.png"
+    ],
+    [
+        "sponsor" => "David Sherrin",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/coldwell-banker-heritage-realtors-240x300.jpg"
+    ],
+    [
+        "sponsor" => "Xandraesign",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/01/xandradesign.png"
+    ],
+    [
+        "sponsor" => "Dayton Chess Club",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/Dayton-Chess-Club-300x82.png"
+    ],
+    [
+        "sponsor" => "Sticker Giant",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/stickergiant-logo-300x199.png"
+    ],
+    [
+        "sponsor" => "Bluehost",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/bluehost-logo13-300x86.png"
+    ],
+    [
+        "sponsor" => "WiredTree",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/wiredtreedayton.png"
+    ],
+    [
+        "sponsor" => "Media Temple",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/mt-250x140-dk.png"
+    ],
+    [
+        "sponsor" => "Code Poet",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/Code-Poet-2.png"
+    ],
+    [
+        "sponsor" => "Dream Host",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/dreamhost_logo-cmyk-no_tag-2012-300x55.jpg"
+    ],
+    [
+        "sponsor" => "WPML",
+        "image" => "http://2014.dayton.wordcamp.org/files/2014/02/wpml.png"
     ]
 ];
