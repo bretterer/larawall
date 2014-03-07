a2794d6032923341bfe5035afed187cd9bc0dedf
Brian Retterer <bretterer@gmail.com>
2014-03-07 17:32:46 -0500

    update schedule

--- app/config/schedule.php
+++ app/config/schedule.php
@@ -3,7 +3,6 @@
 return [
     [
         'data_time' => '0800',
-        'end_time' => '0900',
         'time' => '8:00 AM',
         "sessions" => [
             [
@@ -16,41 +15,260 @@ return [
         ]
     ],
     [
-        'data_time' => '0930',
-        'time' => '9:30 AM',
+        'data_time' => '0900',
+        'time' => '9:00 AM',
         "sessions" => [
             [
-                "title" => "Earl Gregorich",
-                "description" => "Intro to Wordpress",
+                "title" => "Welcome",
+                "description" => "Welcome to #wcdayton",
                 "room" => "Room 160",
                 "track" => "",
-                "image" => "http://0.gravatar.com/avatar/2d441aa835c2a010ff07c5723fd0c5e1?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+                "image" => "http://2014.dayton.wordcamp.org/files/2014/01/wcday2014-badge-attending-large.jpg"
             ]
         ]
     ],
     [
-        'data_time' => '1200',
-        'time' => '12:00 PM',
+        'data_time' => '1000',
+        'time' => '10:00 AM',
         "sessions" => [
             [
-                "title" => "Lunch!",
-                "description" => "Cousin Vinnys",
-                "room" => "Lobby",
-                "track" => "",
-                "image" => "http://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg"
+                "title" => "John Parkinson",
+                "description" => "Find WordPress help",
+                "room" => "Room 160",
+                "track" => "Publisher",
+                "image" => "http://1.gravatar.com/avatar/bc54889b6b19475357652e51986cf55a?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+            ],
+            [
+                "title" => "Tim Yow",
+                "description" => "The Traffic Data that Matters in Google Analytics",
+                "room" => "Room 166",
+                "track" => "Power User",
+                "image" => "http://0.gravatar.com/avatar/07a38eef291ea5a1f7ba1231da508e5a?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+            ],
+            [
+                "title" => "Marc Benzakein",
+                "description" => "There’s No Place Like 127.0.0.1",
+                "room" => "Room 167",
+                "track" => "Developer",
+                "image" => "http://1.gravatar.com/avatar/984914bb5883380eca5d7cdb8fd8ab89?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
             ]
         ]
     ],
     [
-        'data_time' => '1330',
-        'time' => '1:30 PM',
+        'data_time' => '1045',
+        'end_time' => '1051',
+        'time' => '10:45 AM',
         "sessions" => [
             [
-                "title" => "Earl Gregorich",
-                "description" => "Intro to Wordpress",
+                "title" => "Break",
+                "description" => "Welcome to #wcdayton",
+                "room" => "",
+                "track" => "",
+                "image" => "/images/break1.jpg"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1100',
+        'time' => '11:00 AM',
+        "sessions" => [
+            [
+                "title" => "Joseph Herbrandson",
+                "description" => "WordPress Security",
+                "room" => "Room 160",
+                "track" => "Publisher",
+                "image" => "http://1.gravatar.com/avatar/9c49697c8124e6d24bed4e843fec6755?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+            ],
+            [
+                "title" => "Joe Rozsa",
+                "description" => "Everyone is allowed in the kitchen, but not the liquor cabinet",
+                "room" => "Room 166",
+                "track" => "Power User",
+                "image" => "http://1.gravatar.com/avatar/da3d4e7e92dd3c7e5d0bbc01dbd2357c?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Brian Retterer",
+                "description" => "git commit -m “My WordPress blog under version control”",
+                "room" => "Room 167",
+                "track" => "Developer",
+                "image" => "http://0.gravatar.com/avatar/e18fbc1a81c7b4d2264e1fabdd241e15?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1145',
+        'end_time' => '1215',
+        'time' => '11:45 AM',
+        "sessions" => [
+            [
+                "title" => "Lunch",
+                "description" => "Scratch Food",
+                "room" => "scratchfood.com",
+                "track" => "",
+                "image" => "http://www.scratchfood.com/wp-content/uploads/2013/10/Scratch-Event-Catering3-300x98.jpg"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1300',
+        'time' => '1:00 PM',
+        "sessions" => [
+            [
+                "title" => "Cory Miller",
+                "description" => "Keynote",
                 "room" => "Room 160",
                 "track" => "",
-                "image" => "http://0.gravatar.com/avatar/2d441aa835c2a010ff07c5723fd0c5e1?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+                "image" => "http://0.gravatar.com/avatar/ae0202e6f7f7b3edcec844692f0c40de?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1345',
+        'end_time' => '1350',
+        'time' => '1:45 PM',
+        "sessions" => [
+            [
+                "title" => "Break",
+                "description" => "#wcdayton",
+                "room" => "",
+                "track" => "",
+                "image" => "/images/break2.jpg"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1400',
+        'time' => '2:00 PM',
+        "sessions" => [
+            [
+                "title" => "Angela Bergmann",
+                "description" => "Tips & Tricks to make sure your website is accessible",
+                "room" => "Room 160",
+                "track" => "Publisher",
+                "image" => "http://1.gravatar.com/avatar/bcbdb35abbc8fe1236f4d2d7e3a6614b?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Nathan Driver",
+                "description" => "Setting up SEO in WordPress",
+                "room" => "Room 166",
+                "track" => "Power User",
+                "image" => "http://1.gravatar.com/avatar/d0e1f82e58afbc7a32e7f3e1dcf83c64?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Ben Byrne",
+                "description" => "Front-end Performance Optimization",
+                "room" => "Room 167",
+                "track" => "Developer",
+                "image" => "http://1.gravatar.com/avatar/7c7212e5c1127458f0b14758d44f7ac4?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1445',
+        'end_time' => '1450',
+        'time' => '2:45 PM',
+        "sessions" => [
+            [
+                "title" => "Break",
+                "description" => "#wcdayton",
+                "room" => "",
+                "track" => "",
+                "image" => "http://lostgenygirl.files.wordpress.com/2013/05/timthumb.gif"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1500',
+        'time' => '3:00 PM',
+        "sessions" => [
+            [
+                "title" => "Andy Halko",
+                "description" => "Your website deserves more",
+                "room" => "Room 160",
+                "track" => "Publisher",
+                "image" => "http://0.gravatar.com/avatar/8b1cff6e692be3cc8a11c4db33e5345a?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Dustin Filippini",
+                "description" => "Take WordPress Beyond Posts & Pages",
+                "room" => "Room 166",
+                "track" => "Power User",
+                "image" => "http://1.gravatar.com/avatar/79b9d36540d2498042162d8897db3bfc?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "John Hartley",
+                "description" => "Loops on Loops",
+                "room" => "Room 167",
+                "track" => "Developer",
+                "image" => "http://0.gravatar.com/avatar/c47c6ee4a6a6d4db0f128d74aed12e5f?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1545',
+        'end_time' => '1550',
+        'time' => '3:45 PM',
+        "sessions" => [
+            [
+                "title" => "Break",
+                "description" => "#wcdayton",
+                "room" => "",
+                "track" => "",
+                "image" => "http://a.tgcdn.net/images/action/large/2197680.jpg"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1600',
+        'time' => '4:00 PM',
+        "sessions" => [
+            [
+                "title" => "Nile Flores",
+                "description" => "Podcasting for WordPress",
+                "room" => "Room 160",
+                "track" => "Publisher",
+                "image" => "http://0.gravatar.com/avatar/2181809e6ab7ff2dd3f1e12777952255?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Chip Bennett",
+                "description" => "Portable Social Profile Links Using Custom Nav Menus",
+                "room" => "Room 166",
+                "track" => "Power User",
+                "image" => "http://1.gravatar.com/avatar/1299e8d336a2d5faa4fc440d07ea44f8?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ],
+            [
+                "title" => "Ian Dunn",
+                "description" => "The Right Way to Customize a Plugin",
+                "room" => "Room 167",
+                "track" => "Developer",
+                "image" => "http://1.gravatar.com/avatar/d0e2ea89fb62350b1304c62eeeaf24b6?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1645',
+        'end_time' => '1648',
+        'time' => '4:45 PM',
+        "sessions" => [
+            [
+                "title" => "Wrap-up",
+                "description" => "#wcdayton",
+                "room" => "160",
+                "track" => "",
+                "image" => "http://2014.dayton.wordcamp.org/files/2014/01/wcday2014-badge-attending-large.jpg"
+            ]
+        ]
+    ],
+    [
+        'data_time' => '1800',
+        'time' => '6:00 PM',
+        "sessions" => [
+            [
+                "title" => "After Party!",
+                "description" => "#wcdayton",
+                "room" => "The Pub @ The Greene",
+                "track" => "",
+                "image" => "http://www.experiencethepub.com/sites/experiencethepub.com/files/imagecache/600x300_Blog_Image/images/blog/pub_tampa.jpg"
             ]
         ]
     ]
--- app/config/schedule2.php
+++ app/config/schedule2.php
@@ -3,6 +3,7 @@
 return [
     [
         'data_time' => '0800',
+        'end_time' => '0900',
         'time' => '8:00 AM',
         "sessions" => [
             [
@@ -10,74 +11,46 @@ return [
                 "description" => "Welcome to #wcdayton",
                 "room" => "",
                 "track" => "",
-                "image" => ""
+                "image" => "http://2014.dayton.wordcamp.org/files/2014/01/wcday2014-badge-attending-large.jpg"
             ]
         ]
     ],
     [
-        'data_time' => '0900',
-        'time' => '9:00 AM',
+        'data_time' => '0930',
+        'time' => '9:30 AM',
         "sessions" => [
             [
-                "title" => "Welcome",
-                "description" => "Welcome to #wcdayton",
-                "room" => "Room 106",
+                "title" => "Earl Gregorich",
+                "description" => "Intro to Wordpress",
+                "room" => "Room 160",
                 "track" => "",
-                "image" => ""
+                "image" => "http://0.gravatar.com/avatar/2d441aa835c2a010ff07c5723fd0c5e1?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
             ]
         ]
     ],
     [
-        'data_time' => '1000',
-        'time' => '10:00 AM',
+        'data_time' => '1200',
+        'time' => '12:00 PM',
         "sessions" => [
             [
-                "title" => "John Parkinson",
-                "description" => "Find WordPress help",
-                "room" => "Room 106",
-                "track" => "Publisher",
-                "image" => "http://1.gravatar.com/avatar/bc54889b6b19475357652e51986cf55a?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
-            ],
-            [
-                "title" => "Tim Yow",
-                "description" => "The Traffic Data that Matters in Google Analytics",
-                "room" => "Room 108",
-                "track" => "Power User",
-                "image" => "http://0.gravatar.com/avatar/07a38eef291ea5a1f7ba1231da508e5a?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
-            ],
-            [
-                "title" => "Marc Benzakein",
-                "description" => "There’s No Place Like 127.0.0.1",
-                "room" => "Room 110",
-                "track" => "Developer",
-                "image" => "http://1.gravatar.com/avatar/984914bb5883380eca5d7cdb8fd8ab89?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+                "title" => "Lunch!",
+                "description" => "Cousin Vinnys",
+                "room" => "Lobby",
+                "track" => "",
+                "image" => "http://upload.wikimedia.org/wikipedia/commons/d/d3/Supreme_pizza.jpg"
             ]
         ]
     ],
     [
-        'data_time' => '1100',
-        'time' => '11:00 AM',
+        'data_time' => '1330',
+        'time' => '1:30 PM',
         "sessions" => [
             [
-                "title" => "Joseph Herbrandson",
-                "description" => "WordPress Security",
-                "room" => "Room 106",
-                "track" => "Publisher",
-                "image" => "http://1.gravatar.com/avatar/9c49697c8124e6d24bed4e843fec6755?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
-            ],
-            [
-                "title" => "Chip Bennett",
-                "description" => "Portable Social Profile Links Using Custom Nav Menus",
-                "room" => "Room 108",
-                "track" => "Power User",
-                "image" => "http://1.gravatar.com/avatar/1299e8d336a2d5faa4fc440d07ea44f8?s=700&d=http%3A%2F%2F1.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
-            ],
-            [
-                "title" => "Brian Retterer",
-                "description" => "git commit -m “My WordPress blog under version control”",
-                "room" => "Room 110",
-                "track" => "Developer",
-                "image" => "http://0.gravatar.com/avatar/e18fbc1a81c7b4d2264e1fabdd241e15?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&r=G"
+                "title" => "Earl Gregorich",
+                "description" => "Intro to Wordpress",
+                "room" => "Room 160",
+                "track" => "",
+                "image" => "http://0.gravatar.com/avatar/2d441aa835c2a010ff07c5723fd0c5e1?s=700&d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D100&r=G"
             ]
         ]
     ]
